2bde04afa46a6570606716ec50735c47076a9e2b
Siu Kit Lui <colgatelui@gmail.com>
2026-09-03 00:21:45 +0800

    version 1.01

--- HorseRacingAnalysis/runserver.py
+++ HorseRacingAnalysis/runserver.py
@@ -1,14 +1,16 @@
 """
 This script runs the HorseRacingAnalysis application using a development server.
 """
+import os
+from HorseRacingAnalysis import app # 確保指向您的 Flask app 實例
 
-from os import environ
-from HorseRacingAnalysis import app
+# 💡 Vercel 需要讀取名為 app 或 application 的變數
+app = app 
 
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
+    HOST = os.environ.get('SERVER_HOST', 'localhost')
     try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
+        PORT = int(os.environ.get('SERVER_PORT', '5555'))
     except ValueError:
         PORT = 5555
     app.run(HOST, PORT)
